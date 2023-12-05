b4ad02fd9b671acaa9aa1085d9b044c8486ed563
afkdev8 <mr.infinidy@protonmail.com>
2023-12-05 18:32:32 +0100

    Fix context menu on click

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,16 @@
-const clickOnContextMenu = (clickedItem) => {
+
+var searchThis = {
+    "id": "selectedText",
+    "title": "Search on YouTube",
+    contexts: ["selection"],
+};
+
+const contextMenuOnClick = (clickedItem) => {
     var searchTerm = encodeURI(clickedItem.selectionText);
     searchTerm = searchTerm.replace(/\#/g, '%23');
     var address = "https://www.youtube.com/results?search_query=" + searchTerm;
     window.open(address, "_blank");
 }
 
-var searchThis = {
-    "id": "selectedText",
-    "title": "Search on YouTube",
-    contexts: ["selection"],
-    onclick: clickOnContextMenu
-};
-
 chrome.contextMenus.create(searchThis);
+chrome.contextMenus.onClicked.addListener(contextMenuOnClick);
